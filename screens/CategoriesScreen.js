4186a9aeba066243bc909d1beb15b343effbd774
Brian Berg <bberg11@gmail.com>
2020-05-12 12:18:24 -0400

    Output categories dummy data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
+
+const CategoriesScreen = props => {
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
+  }
 });
-
-const CategoriesScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button
-        title="Go To Category Meals"
-        onPress={() =>
-          props.navigation.navigate({ routeName: 'CategoryMeals' })
-        }
-      />
-    </View>
-  );
-};
 
 export default CategoriesScreen;
