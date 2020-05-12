61909c47950a7a5779a60c4bf0609845225f538b
Brian Berg <bberg11@gmail.com>
2020-05-12 13:42:45 -0400

    Pass meal data to detail screen and update header and text

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,5 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import { MEALS } from '../data/dummy-data';
+
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <View style={styles.screen}>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  console.log(selectedMeal);
+  return {
+    headerTitle: selectedMeal.title,
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
@@ -8,13 +37,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-const MealDetailScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Meal Detail Screen</Text>
-    </View>
-  );
-};
 
 export default MealDetailScreen;
