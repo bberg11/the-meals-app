0f0e25c2066f89046fa32153c5d13e65a1813572
Brian Berg <bberg11@gmail.com>
2020-05-12 12:40:30 -0400

    Dynamically load navigation options for CategoryMealsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,5 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
+const CategoryMealScreen = (props) => {
+  const catId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail',
+          });
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          props.navigation.pop();
+        }}
+      />
+    </View>
+  );
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
@@ -9,16 +53,4 @@ const styles = StyleSheet.create({
   },
 });
 
-const CategoryMealsScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Category Meals Screen</Text>
-      <Button
-        title="Go to Meal Detail"
-        onPress={() => props.navigation.navigate({ routeName: 'MealDetail' })}
-      />
-    </View>
-  );
-};
-
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
