b921b9c6cd2d9db393681a35d2c3ed3c39fa0c22
Brian Berg <bberg11@gmail.com>
2020-05-12 22:37:17 -0400

    Add "Favorite" star button to header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
@@ -24,9 +26,19 @@ const MealDetailScreen = (props) => {
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam('mealId');
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
-  console.log(selectedMeal);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log('Mark as favorite!');
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
